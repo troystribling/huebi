6bf52493f97633c5e55e52b6b938237900fa7a94
Troy Stribling <troy.stribling@gmail.com>
2014-11-16 10:26:07 -0800

    created project

---
+++ huebi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  huebi
+//
+//  Created by Troy Stribling on 11/16/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
