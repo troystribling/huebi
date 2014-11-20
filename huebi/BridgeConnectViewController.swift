569696db3ad5d94df35b44934899fb92449d012d
Troy Stribling <troy.stribling@gmail.com>
2014-11-20 09:05:19 -0800

    more work on bridge connect

---
+++ huebi/BridgeConnectViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BridgeConnectViewController.swift
+//  huebi
+//
+//  Created by Troy Stribling on 11/20/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+import UIKit
+
+class BridgeConnectViewController: UIViewController {
+
+    @IBOutlet var bridgeConnectProgressView : UIProgressView!
+    
+    
+    required init(coder aDecoder:NSCoder) {
+        super.init(coder:aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    }
+
+}
